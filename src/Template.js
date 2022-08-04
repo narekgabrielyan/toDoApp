577bbad4966ce0dc9cdff02ba2dc4ecbc8b8f730
narek.gabrielyan <narek.gabrielyan@digitain.com>
2022-08-04 17:33:57 +0400

    chore: font prefix updated

--- src/Template.js
+++ src/Template.js
@@ -5,9 +5,9 @@ class Template {
                 <li class="${i.completed ? 'list_item list_item--done' : 'list_item'}" id="${i.id}">
                     <div class="list_item_content">
                         <input type="checkbox" class="toggle_item" id="toggle${i.id}" ${i.completed ? 'checked' : ''}>
-                        <label for="toggle${i.id}" class="toggle_item-checkbox lv_icon--unchecked"></label>
+                        <label for="toggle${i.id}" class="toggle_item-checkbox icon-unchecked"></label>
                         <span class="list_item_title">${i.title}</span>
-                        <button class="list_item_cancel-btn lv_icon--cancel"></button>
+                        <button class="list_item_cancel-btn icon-cancel"></button>
                     </div>
                 </li>
         `}, '');
