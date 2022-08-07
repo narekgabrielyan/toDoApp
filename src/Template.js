df6236f933b3d83762ec209d72bc2c30bda44dff
narek.gabrielyan <narek.gabrielyan@digitain.com>
2022-08-07 14:10:12 +0400

    chore: css rules were substituted with helper classnames

--- src/Template.js
+++ src/Template.js
@@ -3,12 +3,12 @@ export default class Template {
         return items.reduce((a, i) => {
             return a + `
                 <li class="${i.completed ? 'list_item list_item-done' : 'list_item'}" id="${i.id}">
-                    <div class="list_item_content">
-                        <input type="checkbox" class="toggle_item" id="toggle${i.id}" ${i.completed ? 'checked' : ''}>
-                        <label for="toggle${i.id}" class="checkbox-toggle_item"></label>
+                    <div class="list_item_content flex align-center justify-center">
+                        <input type="checkbox" class="toggle_item pointer" id="toggle${i.id}" ${i.completed ? 'checked' : ''}>
+                        <label for="toggle${i.id}" class="checkbox-toggle_item pointer"></label>
                         <span class="list_item_title" title="${i.title}">${i.title}</span>
-                        <button class="btn-list_item btn-list_item-edit icon-edit flex align-center justify-center" title="Click to edit"></button>
-                        <button class="btn-list_item btn-list_item-cancel icon-remove flex align-center justify-center" title="Click to remove"></button>
+                        <button class="btn-list_item btn-list_item-edit icon-edit flex align-center justify-center pointer" title="Click to edit"></button>
+                        <button class="btn-list_item btn-list_item-cancel icon-remove flex align-center justify-center pointer" title="Click to remove"></button>
                     </div>
                 </li>
         `}, '');
