5d63202d9de88c1510df5e976ca818bf028b12f9
narek.gabrielyan <narek.gabrielyan@digitain.com>
2022-08-16 23:56:12 +0400

    chore: removed constant from reduce in template

--- src/js/Template.js
+++ src/js/Template.js
@@ -3,13 +3,12 @@ import {escapeForHTML, escapeForHTMLAttributes} from "./utils";
 export default class Template {
     itemsList(items) {
         return items.reduce((a, i) => {
-            const title = escapeForHTML(i.title);
             return a + `
                 <li class="${i.completed ? 'list_item list_item-done' : 'list_item'}" id="${i.id}">
                     <div class="list_item_content flex align-center justify-center">
                         <input type="checkbox" class="toggle_item pointer" id="toggle${i.id}" ${i.completed ? 'checked' : ''}>
                         <label for="toggle${i.id}" class="checkbox-toggle_item pointer"></label>
-                        <span class="list_item_title" title="${escapeForHTMLAttributes(title)}">${title}</span>
+                        <span class="list_item_title" title="${escapeForHTMLAttributes(i.title)}">${escapeForHTML(i.title)}</span>
                         <button class="btn-list_item btn-list_item-edit icon-edit flex align-center justify-center pointer" title="Click to edit"></button>
                         <button class="btn-list_item btn-list_item-cancel icon-remove flex align-center justify-center pointer" title="Click to remove"></button>
                     </div>
