d7391822f81313f2f701a8beac0465ccdf31fd95
narek.gabrielyan <narekgabrielyan998@gmail.com>
2022-10-15 12:18:50 +0400

    chore: login api refactored

--- src/index.js
+++ src/index.js
@@ -3,39 +3,52 @@ import Template from './js/Template';
 import View from './js/View';
 import Controller from './js/Controller';
 import './sass/all.scss';
-import {getUserLoggedIn} from "./api/services/login";
-import {PAGE_PATHS} from "./js/constants";
-import {changeLocationTo} from "./js/utils";
+import { getUserLoggedIn } from './api/services/login';
+import { PAGE_PATHS } from './js/constants';
+import {
+  changeLocationTo,
+  createEl,
+  outsideClickHandler,
+  setDataToLocalStorage,
+} from './js/utils';
 
 const onLogin = () => {
-    const username = document.forms['loginForm']['username'].value;
-    const password = document.forms['loginForm']['password'].value;
-    getUserLoggedIn(username, password, (result) => {
-        localStorage.setItem('sessionData', result);
-        changeLocationTo(PAGE_PATHS.INDEX);
-    });
-}
+  const username = document.forms['loginForm']['username'].value;
+  const password = document.forms['loginForm']['password'].value;
+  getUserLoggedIn(username, password, (result) => {
+    if (result?.error) {
+      const errorMsg = createEl('span', {
+        innerText: 'Invalid username or password',
+        className: 'error_msg',
+      });
+      document.getElementById('loginFormContent').appendChild(errorMsg);
+      outsideClickHandler(errorMsg, () => errorMsg.remove());
+    } else {
+      setDataToLocalStorage('userData', result);
+      changeLocationTo(PAGE_PATHS.INDEX);
+    }
+  });
+};
 
 const renderAppPage = () => {
-    const token = JSON.parse(localStorage.getItem('sessionData'))?.token;
-    if(!token) {
-        if(window.location.pathname !== PAGE_PATHS.LOGIN) {
-            changeLocationTo(PAGE_PATHS.LOGIN)
-        }
-        document.getElementById('login_btn').addEventListener('click', onLogin);
-
-    } else {
-        changeLocationTo(PAGE_PATHS.INDEX)
-        const store = new Store('todoList');
-        const template = new Template();
-        const view = new View(template);
-        const controller = new Controller(view, store);
-
-        const setView = () => controller.setView(document.location.hash);
-
-        window.addEventListener('load', setView);
-        window.addEventListener('hashchange', setView);
+  const token = JSON.parse(localStorage.getItem('userData'))?.token;
+  if (!token) {
+    if (window.location.pathname !== PAGE_PATHS.LOGIN) {
+      changeLocationTo(PAGE_PATHS.LOGIN);
     }
-}
+    document.getElementById('login_btn').addEventListener('click', onLogin);
+  } else {
+    changeLocationTo(PAGE_PATHS.INDEX);
+    const store = new Store('todoList');
+    const template = new Template();
+    const view = new View(template);
+    const controller = new Controller(view, store);
+
+    const setView = () => controller.setView(document.location.hash);
+
+    window.addEventListener('load', setView);
+    window.addEventListener('hashchange', setView);
+  }
+};
 
 renderAppPage();
