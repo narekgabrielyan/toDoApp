6a3f55760b1c6b07670f1cc38b0f7c4d75dd43fb
narek.gabrielyan <narekgabrielyan998@gmail.com>
2022-10-16 23:54:24 +0400

    chore: account markup draft added

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,37 @@
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
-    entry: './src/index.js',
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist'),
-        publicPath: '/',
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: 'src/index.html',
-            filename: 'index.html',
-        }),
-        new HtmlWebpackPlugin({
-            template: 'src/login.html',
-            filename: 'login.html',
-        }),
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+      filename: 'index.html',
+    }),
+    new HtmlWebpackPlugin({
+      template: 'src/login.html',
+      filename: 'login.html',
+    }),
+    new HtmlWebpackPlugin({
+      template: 'src/account.html',
+      filename: 'account.html',
+    }),
+  ],
+  devServer: {
+    static: './dist',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(scss|css)$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
     ],
-    devServer: {
-        static: './dist',
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(scss|css)$/,
-                use: ['style-loader', 'css-loader', 'sass-loader'],
-            },
-        ],
-    },
-}
+  },
+};
